978e8e2eaf933576a014bee16fab57f9fcfc40fd
Sina Rismanchian <sina.rismanchian@gmail.com>
2024-08-06 16:00:39 -0700

    update

---
+++ simulation_settings.py
@@ -0,0 +1,138 @@
+setting_default = {
+    "passive": {
+        "name": "passive",
+        "order": {
+            "passive": 1
+        },
+        "count": 50
+    },
+    "active": {
+        "name": "active",
+        "order": {
+            "active": 1
+        },
+        "count": 50
+    },
+    "constructive": {
+        "name": "constructive",
+        "order": {
+            "constructive": 1
+        },
+        "count": 50
+    },
+    "interactive": {
+        "name": "interactive",
+        "order": {
+            "interactive": 1
+        },
+        "count": 50
+    }
+}
+
+mceldoon = {
+    "Observed lessFP": {
+        "name": "Observed lessFP",
+        "order": {
+            "active": 0.5,
+            # "active": 1,
+            # "constructive": 1
+        },
+        "count": 50
+    },
+    "Ideal lessFP": {
+        "name": "Ideal lessFP",
+        "order": {
+            "active": 1,
+            # "constructive": 1
+        },
+        "count": 50
+    },
+    "Observed moreFP": {
+        "name": "Observed moreFP",
+        "order": {
+            "constructive": 0.5,
+            "active": 1
+        },
+        "count": 50
+    },
+     "Ideal moreFP": {
+        "name": "Ideal moreFP",
+        "order": {
+            "constructive": 1
+        },
+        "count": 50
+    }
+    # "constructive": {
+    #     "name": "constructive",
+    #     "order": {
+    #         "constructive": 1
+    #     },
+    #     "count": 50
+    # },
+    # "interactive": {
+    #     "name": "interactive",
+    #     "order": {
+    #         "interactive": 1
+    #     },
+    #     "count": 50
+    # }
+}
+
+
+expertise_reversal = {
+    "Novice Learner": {
+        "name": "Novice Learner",
+        "order": {
+            "active": 0.5,
+            # "active": 1,
+            # "constructive": 1
+        },
+        "count": 50
+    },
+    "Ideal lessFP": {
+        "name": "Ideal lessFP",
+        "order": {
+            "active": 1,
+            # "constructive": 1
+        },
+        "count": 50
+    },
+    "Observed moreFP": {
+        "name": "Observed moreFP",
+        "order": {
+            "constructive": 0.5,
+            "active": 1
+        },
+        "count": 50
+    },
+     "Ideal moreFP": {
+        "name": "Ideal moreFP",
+        "order": {
+            "constructive": 1
+        },
+        "count": 50
+    }
+    # "constructive": {
+    #     "name": "constructive",
+    #     "order": {
+    #         "constructive": 1
+    #     },
+    #     "count": 50
+    # },
+    # "interactive": {
+    #     "name": "interactive",
+    #     "order": {
+    #         "interactive": 1
+    #     },
+    #     "count": 50
+    # }
+}
+
+
+
+default_increments = {
+  'passive': 0.05,
+  'active': 0.1,
+  'constructive': 0.05,
+  'interactive': 0.1
+}
